4e22d96f6184c0b1e2fa8ef15e0a3df7ab88e850
Amelia Boli <ameliaaboli@gmail.com>
2016-04-14 23:51:37 -0700

    feat: Storyboard set up and UIImagePickerController works

--- ImagePickerSample/ViewController.swift
+++ ImagePickerSample/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickedImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pickImage(sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        self.presentViewController(pickerController, animated: true, completion: nil)
     }
-
-
 }
 
